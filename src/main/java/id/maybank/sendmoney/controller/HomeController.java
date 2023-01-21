63f74e4f3f27c1c41cbd7d79e814f56f4353e00f
rzlwbo1 <rizalwb2@gmail.com>
2023-01-21 17:18:19 +0700

    cek rekening penerima & pengirim

--- src/main/java/id/maybank/sendmoney/controller/HomeController.java
+++ src/main/java/id/maybank/sendmoney/controller/HomeController.java
@@ -1,16 +1,33 @@
 package id.maybank.sendmoney.controller;
 
+import id.maybank.sendmoney.entity.Rekening;
+import id.maybank.sendmoney.service.rekening.RekeningService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private RekeningService rekeningService;
+
     @GetMapping
     public String index() {
         return "home";
+    }
+
+    @GetMapping("/cek-rek/{norek}")
+    public String cekRek(@PathVariable String norek, Model model) {
+
+        Rekening infoRek = this.rekeningService.findByNoRek(norek);
+
+        model.addAttribute("rekeningInfo", infoRek);
+        return "cek-rekening";
     }
 
 }
--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,4 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    /*<![CDATA[*/
+        var baseUrl = /*[[@{/}]]*/ "";
+    /*]]>*/
+
+
+    const linkRek = document.getElementById("linkRekPengirim");
+    const selectRek = document.getElementById("selRekPengirim");
+
+    selectRek.addEventListener("input", (evt) => {
+
+      let optionRek = evt.target.options;
+
+      let norek = optionRek[optionRek.selectedIndex].text;
+      cekNorek(norek)
+      console.log(norek)
+
+    })
+
+    linkRek.addEventListener("click", (evt) => {
+      // evt.preventDefault();
+      cekNorek();
+
+    })
+
+    function cekNorek(norek) {
+
+      // norek awal
+      if (!norek) {
+
+        let rekAwal = selectRek.options[selectRek.options.selectedIndex].text;
+        linkRek.setAttribute("href", "/cek-rek/"+rekAwal);
+
+        return
+      }
+
+      linkRek.setAttribute("href", "/cek-rek/"+norek);
+
+    }
+
+
   (document.querySelectorAll('.notification .delete') || []).forEach(del => {
     const notification = del.parentNode;
 
@@ -6,6 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
       notification.parentNode.removeChild(notification);
     });
   });
+
+
 });
 
 console.log("ini js");
